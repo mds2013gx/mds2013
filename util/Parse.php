5acf36501df894794356fbf6688c85306a7a9e5e
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-10-12 08:46:04 -0300

    Implementacao do metodo parsePorSerieHistorica sem teste

--- util/Parse.php
+++ util/Parse.php
@@ -24,8 +24,45 @@ class Parse{
 	}
 	
 	function parseDeSerieHistorica(){
-		
-	}
+		$numeroLinhas = 40;
+		$numeroColunas = 14;
+		$numeroColunas = $this->dados->colcount();
+		$natureza[];
+		$tempo[];
+		$crime[natureza][tempo][dado];
+		//loop que pega natureza do crime
+		for($i=0; $i<$numeroLinhas; $i++){
+			if(($i == 5)||($i == 21)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
+				$i++;
+			}
+			else{	
+				$natureza[$i]= $this->dados->val($i, C);
+			}
+		}
+		//loop que pega os anos disponiveis
+		for($i=0; $i<$numeroColunas; $i++){
+			if(($i == 1)||($i == 2)||($i == 3)){
+				$i++;
+			}else{
+				$tempo[$i] = $this->dados->val(1, $i);
+			}
+		}
+		//loop que pega os dados do crime
+		for($i=0; $i<strlen($natureza); $i++){
+				if(($i == 5)||($i == 21)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
+					$i++;
+				}else{
+					for($j=0; $j=strlen($tempo); $i++){
+						if(($j == 1)||($j == 2)||($j == 3)){
+							$j++;
+						}else{
+							$crime[$i][$j][$this->dados->val($i,$j)];
+						}
+					}
+				}
+			}
+		}
+	}//fim do metodo parseDeSerieHistorica
 	
 	function parsePorRegiao(){
 		
