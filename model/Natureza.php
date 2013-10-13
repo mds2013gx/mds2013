2cce3b810e8a9f02165bfd7ef59435ef0073e71f
unknown <eliseuegewarth@gmail.com>
2013-10-13 08:35:21 -0300

    Implementacao da classe Natureza

--- model/Natureza.php
+++ model/Natureza.php
@@ -3,4 +3,23 @@ class Natureza{
 	private $idNatureza;
 	private $idCategoria;
 	private $natureza;
+	
+	public function __setIdNatureza($idNatureza){
+		$this->idNatureza = $idNatureza;
+	}
+	public function __getIdNatureza(){
+		return $this->idNatureza;
+	}
+	public function __setIdCategoria($idCategoria){
+		$this->idCategoria = $idCategoria;
+	}
+	public function __getIdCategoria(){
+		return $this->idCategoria;
+	}
+	public function __setNatureza($natureza){
+		$this->natureza = $natureza;
+	}
+	public function __getNatureza(){
+		return $this->natureza;
+	}
 }
