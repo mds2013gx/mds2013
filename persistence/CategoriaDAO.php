cfb7f1e3135be2baf6b5dd1516e55d7973b14190
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2013-10-13 11:31:34 -0300

    Correcao da comunicacao com a classe Conexao.php e adptacao e correcao dos returns do metodo

--- persistence/CategoriaDAO.php
+++ persistence/CategoriaDAO.php
@@ -8,7 +8,7 @@ class CategoriaDAO{
 	
 	public function listarTodas(){
 		$sql = "SELECT * FROM categoria";
-		$resultado = $this->conexao->Execute($sql);
+		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject())
 		{
 			$dadosCategoria = new Categoria();
@@ -21,7 +21,7 @@ class CategoriaDAO{
 	} 
 	public function listarTodasAlfabicamente(){
 		$sql = "SELECT * FROM categoria ORDER BY nome_categoria ASC ";
-		$resultado = $this->conexao->Execute($sql);
+		$resultado = $this->conexao->banco->Execute($sql);
 		while($registro = $resultado->FetchNextObject())
 		{
 			$dadosCategoria = new Categoria();
@@ -34,37 +34,29 @@ class CategoriaDAO{
 	}
 	public function consultarPorId($id){
 		$sql = "SELECT * FROM categoria WHERE id_categoria = $id";
-		$resultado = $this->conexao->Execute($sql);
-		while($registro = $resultado->FetchNextObject())
-		{
-			$dadosCategoria = new Categoria();
-			$dadosCategoria->__setIdCategoria($registro->ID_CATEGORIA);
-			$dadosCategoria->__setNomeCategoria($registro->NOME_CATEGORIA);
-		
-			$retornaCategorias[] = $dadosCategoria;
-		}
-		return $retornaCategorias;
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosCategoria = new Categoria();
+		$dadosCategoria->__setIdCategoria($registro->ID_CATEGORIA);
+		$dadosCategoria->__setNomeCategoria($registro->NOME_CATEGORIA);
+		return $dadosCategoria;
 		
 	}
 	public function consultarPorNome($nomeCategoria){
 		$sql = "SELECT * FROM categoria WHERE nome_categoria = $nomeCategoria";
-		$resultado = $this->conexao->Execute($sql);
-		while($registro = $resultado->FetchNextObject())
-		{
-			$dadosCategoria = new Categoria();
-			$dadosCategoria->__setIdCategoria($registro->ID_CATEGORIA);
-			$dadosCategoria->__setNomeCategoria($registro->NOME_CATEGORIA);
-		
-			$retornaCategorias[] = $dadosCategoria;
-		}
-		return $retornaCategorias;
+		$resultado = $this->conexao->banco->Execute($sql);
+		$registro = $resultado->FetchNextObject();
+		$dadosCategoria = new Categoria();
+		$dadosCategoria->__setIdCategoria($registro->ID_CATEGORIA);
+		$dadosCategoria->__setNomeCategoria($registro->NOME_CATEGORIA);
+		return $dadosCategoria;
 	}
 	public function inserirCategoria($arrayCategoria){
 		$dadosCategoria = new Categoria();
 		for($i=0; $i<count(arrayCategoria);$i++){
 			$dadosCategoria->__setNomeCategoria($arrayCategoria[$i]);
 			$sql = "INSERT INTO nome_categoria values ('{$dadosCategoria->__getNomeCategoria()}')";
-			$this->conexao->Execute($sql);
+			$this->conexao->banco->Execute($sql);
 		}
 	}
 }
