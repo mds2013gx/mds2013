bf96dd5277b60033c3cf9f2ca78bae6bc355c425
unknown <eliseuegewarth@gmail.com>
2013-10-13 08:28:57 -0300

    Implementacao das classes RegiaoAdministrativa e Tempo da model

--- model/RegiaoAdministrativa.php
+++ model/RegiaoAdministrativa.php
@@ -2,4 +2,17 @@
 class RegiaoAdministrativa{
 	private $idRegiaoAdministrativa;
 	private $nomeRegiao;
+	
+	public function __setIdRegiaoAdministrativa($idRegiaoAdministrativa){
+		$this->idRegiaoAdministrativa = $idRegiaoAdministrativa;
+	}
+	public function __getIdRegiaoAdministrativa(){
+		return $this->idRegiaoAdministrativa;
+	}
+	public function __setNomeRegiao($nomeRegiao){
+		$this->nomeRegiao = $nomeRegiao;
+	}
+	public function __getNomeRegiao(){
+		return $this->nomeRegiao;
+	}
 }
