1b43f2c34edfba93d9eda111178558c74e6e8464
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2013-10-14 17:20:35 -0300

    Atualizacao de MER e script sql do banco

--- files/script_banco.sql
+++ files/script_banco.sql
@@ -1,9 +1,3 @@
-CREATE TABLE regiao_administrativa (
-  id_regiao_administrativa INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome_regiao INTEGER UNSIGNED NULL,
-  PRIMARY KEY(id_regiao_administrativa)
-);
-
 CREATE TABLE tempo (
   id_tempo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   intervalo INTEGER UNSIGNED NULL,
@@ -27,12 +21,10 @@ CREATE TABLE natureza (
 CREATE TABLE crime (
   id_crime INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   tempo_id_tempo INTEGER UNSIGNED NOT NULL,
-  regiao_administrativa_id_regiao_administrativa INTEGER UNSIGNED NOT NULL,
   natureza_id_natureza INTEGER UNSIGNED NOT NULL,
   quantidade INTEGER UNSIGNED NULL,
   PRIMARY KEY(id_crime),
   INDEX crime_FKIndex1(natureza_id_natureza),
-  INDEX crime_FKIndex2(regiao_administrativa_id_regiao_administrativa),
   INDEX crime_FKIndex4(tempo_id_tempo)
 );
 
