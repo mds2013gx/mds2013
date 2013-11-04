33e9e4290ddee8b529dc9d87a6265c39855575d7
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-11-04 10:15:07 -0200

    Implementacao das exceptions no Parse

--- util/Parse.php
+++ util/Parse.php
@@ -1,5 +1,12 @@
 <?php
 require_once 'libs/excel_reader2.php';
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/ENomePlanilhaIncompativel.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/EPlanilhaSerieIncompativel.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/EFalhaLeituraSerieCategoria.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/EFalhaLeituraSerieNatureza.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/EFalhaLeituraSerieTempo.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/exceptions/EFalhaLeituraSerieCrime.php');
+
 class Parse{
 	private $natureza;
 	private $tempo;
@@ -8,86 +15,121 @@ class Parse{
 	private $dados;
 	
 	public function __construct($planilha){
-		$this->dados = new Spreadsheet_Excel_Reader("files/".$planilha,"UTF-8");
-		if($planilha = "série histórica - 2001 - 2012 2.xls"){
-			$this->parseDeSerieHistorica();
+		try{
+			if($planilha = "série histórica - 2001 - 2012 2.xls"){
+				$this->parseDeSerieHistorica();
+			}
+			else if($planilha = "JAN_SET_2011_12  POR REGIAO ADM_2.xls"){
+				$this->parsePorRegiao();
+			}
+			else if($planilha = "Quadrimestre_final.2013"){
+				$this->parseDeQuadrimestre();
+			}
+			else{
+				throw ENomePlanilhaIncompativel();
+			}		
+			$this->dados = new Spreadsheet_Excel_Reader("files/".$planilha,"UTF-8");
 		}
-		else if($planilha = "JAN_SET_2011_12  POR REGIAO ADM_2.xls"){
-			$this->parsePorRegiao();
-		}
-		else{
-			$this->parseDeQuadrimestre();
+		catch(ENomePlanilhaIncompativel $e){
+			echo $e->getMessage();
 		}	
 	}
 	//ParsePorSerieHistorica 
 	public function parseDeSerieHistorica(){
-		$numeroLinhas = 40;
-		$numeroColunas = 15;
-		//loop que pega a natureza
-		for($i=0,$auxCategoria=0;$i<$numeroLinhas;$i++){
-			if($i == 2){
-				$this->categoria[$auxCategoria] = $this->dados->val($i,1,1);
-				$auxCategoria++;
+		try{
+			if($this->dados->val(2, 1,1) != "Natureza"){
+				throw EPlanilhaSerieIncompativel();
 			}
-			if($i == 33){
-				$this->categoria[$auxCategoria] =  $this->dados->val($i,1,1);
-				$auxCategoria++;
-			}
-			if($i == 38){
-				$this->categoria[$auxCategoria] =  $this->dados->val($i,1,1);
-			}
-		} 
-		//loop que pega natureza do crime
-		for($i=1,$auxNatureza=0; $i<$numeroLinhas; $i++){
-			if(($i == 1)||($i == 5)||($i == 21)||($i == 27)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
-				continue;
-			}
-			else{	
-				if($i>32){
-					if($i<37){
-						$this->natureza[$this->__getCategoria()[1]][$auxNatureza]= $this->dados->val($i,'B',1);
-					}else{
-						$this->natureza[$this->__getCategoria()[2]][$auxNatureza]= $this->dados->val($i,'B',1);
-					}
-				}else{
-					if($i<32){
-						$this->natureza[$this->__getCategoria()[0]][$auxNatureza]= $this->dados->val($i,'C',1);
-					}else if($i>32 && $i<37){
-						$this->natureza[$this->__getCategoria()[1]][$auxNatureza]= $this->dados->val($i,'C',1);
-					}else{
-						$this->natureza[$this->__getCategoria()[2]][$auxNatureza]= $this->dados->val($i,'C',1);
-					}
+			$numeroLinhas = 40;
+			$numeroColunas = 15;
+			//loop que pega a natureza
+			for($i=0,$auxCategoria=0;$i<$numeroLinhas;$i++){
+				if($i == 2){
+					$this->categoria[$auxCategoria] = $this->dados->val($i,1,1);
+					$auxCategoria++;
 				}
-				$auxNatureza++;
+				if($i == 33){
+					$this->categoria[$auxCategoria] =  $this->dados->val($i,1,1);
+					$auxCategoria++;
+				}
+				if($i == 38){
+					$this->categoria[$auxCategoria] =  $this->dados->val($i,1,1);
+				}
 			}
-		}
-		//loop que pega os anos disponiveis
-		for($i=1,$auxTempo = 0; $i<$numeroColunas; $i++){
-			if(($i == 1)||($i == 2)||($i == 3)){
-				continue;
-			}else{
-				$this->tempo[$auxTempo] = $this->dados->val(1,$i,1);
-				$auxTempo++;
-			}
-		}
-		//loop que pega os dados do crime
-		for($i=1,$auxLinha=0; $i<$numeroLinhas; $i++){	
-			if(($i == 1)||($i == 5)||($i == 21)||($i == 27)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
-				continue;
-			}else{	
-				for($j=4,$auxColuna=0,$auxCategoria; $j<$numeroColunas; $j++){
-						if($i<32){
-							$auxCategoria = 0;
-						}else if($i>32 && $i<37){
-							$auxCategoria = 1;
+			if(($this->__getCategoria() == null) || (empty($this->__getCategoria())== true) || (count($this->__getCategoria()) != 3)){
+				throw new EFalhaLeituraSerieCategoria();
+			} 
+			//loop que pega natureza do crime
+			for($i=1,$auxNatureza=0; $i<$numeroLinhas; $i++){
+				if(($i == 1)||($i == 5)||($i == 21)||($i == 27)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
+					continue;
+				}
+				else{	
+					if($i>32){
+						if($i<37){
+							$this->natureza[$this->__getCategoria()[1]][$auxNatureza]= $this->dados->val($i,'B',1);
 						}else{
-							$auxCategoria = 2;
+							$this->natureza[$this->__getCategoria()[2]][$auxNatureza]= $this->dados->val($i,'B',1);
 						}
-						$this->crime[$this->__getNatureza()[$this->__getCategoria()[$auxCategoria]][$auxLinha]][$this->__getTempo()[$auxColuna]] = $this->dados->raw($i,$j,1);
-						$auxColuna++;
+					}else{
+						if($i<32){
+							$this->natureza[$this->__getCategoria()[0]][$auxNatureza]= $this->dados->val($i,'C',1);
+						}else if($i>32 && $i<37){
+							$this->natureza[$this->__getCategoria()[1]][$auxNatureza]= $this->dados->val($i,'C',1);
+						}else{
+							$this->natureza[$this->__getCategoria()[2]][$auxNatureza]= $this->dados->val($i,'C',1);
+						}
+					}
+					$auxNatureza++;
 				}
-				$auxLinha++;
 			}
+			if(($this->__getNatureza() == null) || (empty($this->__getNatureza()) == true) || (count($this->__getNatureza()[0]) !=25) || (count($this->__getNatureza()[1]) !=4) || (count($this->__getNatureza()[1]) != 2)){			
+				throw new EFalhaLeituraSerieNatureza();
+			}
+			//loop que pega os anos disponiveis
+			for($i=1,$auxTempo = 0; $i<$numeroColunas; $i++){
+				if(($i == 1)||($i == 2)||($i == 3)){
+					continue;
+				}else{
+					$this->tempo[$auxTempo] = $this->dados->val(1,$i,1);
+					$auxTempo++;
+				}
+			}
+			if(($this->__getTempo() ==  null) || (empty($this->__getTempo())== true) || (count($this->__getTempo()) !=11)){
+				throw EFalhaLeituraSerieTempo();
+			}
+			//loop que pega os dados do crime
+			for($i=1,$auxLinha=0; $i<$numeroLinhas; $i++){	
+				if(($i == 1)||($i == 5)||($i == 21)||($i == 27)||($i == 28)||($i == 31)||($i == 32)||($i == 37)||($i == 40)){
+					continue;
+				}else{	
+					for($j=4,$auxColuna=0,$auxCategoria; $j<$numeroColunas; $j++){
+							if($i<32){
+								$auxCategoria = 0;
+							}else if($i>32 && $i<37){
+								$auxCategoria = 1;
+							}else{
+								$auxCategoria = 2;
+							}
+							$this->crime[$this->__getNatureza()[$this->__getCategoria()[$auxCategoria]][$auxLinha]][$this->__getTempo()[$auxColuna]] = $this->dados->raw($i,$j,1);
+							$auxColuna++;
+					}
+					$auxLinha++;
+				}
+			}
+			if(($this->__getCrime() ==  null) || (empty($this->__getCrime())== true)){
+				throw EFalhaLeituraSerieCrime();
+			}
+		}catch(EPlanilhaSerieIncompativel $e){
+			echo $e->getMessage();
+		}catch (EFalhaLeituraSerieCategoria $e){
+			echo $e->getMessage();
+		}catch(EFalhaLeituraSerieNatureza $e){
+			echo $e->getMessage();
+		}catch(EFalhaLeituraSerieTempo $e){
+			echo $e->getMessage();
+		}catch(EFalhaLeituraSerieCrime $e){
+			echo $e->getMessage();
 		}
 	}//fim do metodo parseDeSerieHistorica
 	
