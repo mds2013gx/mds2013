12ba2e65e31b3fdce31dd6238ecf0f53def07e58
unknown <luks.gl@hotmail.com>
2013-11-05 13:25:12 -0200

    Criacao do diretorio de testes para controller

---
+++ test/controller/CategoriaControllerTeste.php
@@ -0,0 +1,16 @@
+<?php
+	require_once "../controller/CategoriaController.php";
+	
+	/**
+	 * Classe CategoriaControllerTeste
+	 **/
+	class CategoriaControllerTeste extends PHPUnit_Framework_Testcase{
+		
+		public function testListarTodas()
+		{
+			$categoriaController = new CategoriaController();
+			
+		}
+		
+	}
+?>
