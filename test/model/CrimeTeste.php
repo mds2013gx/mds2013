197e93127009a20dcf17136ec79248f5151ac259
Eliseu Egewarth <eliseuegewarth@gmail.com>
2013-11-10 12:57:09 -0200

    Aprimoramento da classe de teste CrimeTeste.php
    implementação de assertivas do atributo IdCrime.

--- test/model/CrimeTeste.php
+++ test/model/CrimeTeste.php
@@ -1,14 +1,41 @@
 <?php
 	/**
+	 * @author Eliseu
 	 * Classe Crime Teste
 	 **/
 	require_once "../../model/Crime.php";
 	
 	class CrimeTeste extends PHPUnit_Framework_Testcase{
-		public function testeIdCrime(){
+		public function testeAtributoIdCrime(){
 			$crime = new Crime();
 			$crime->__setIdCrime(15);
 			$this->assertEquals(15, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime("asdf");
+			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime(14.9);
+			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime($this);
+			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime($crime);
+			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime(10);
+			$this->assertEquals(10, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime(100);
+			$this->assertEquals(100, $crime->__getIdCrime());
+			$crime = new Crime();
+			$crime->__setIdCrime(0);
+			$this->assertEquals(NULL, $crime->__getIdCrime());
+			$this->assertEquals(NULL, 0);
+			$crime = new Crime();
+			$crime->__setIdCrime(50);
+			$this->assertEquals(50, $crime->__getIdCrime());
+				
 		}
 		public function testeQuantidade(){
 			$crime = new Crime();
