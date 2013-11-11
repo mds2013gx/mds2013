77c3e8b3ba041ad3794b13e58ebee5ca9e329c98
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2013-11-11 20:51:54 -0200

    Criacao de Exceptions para classe CrimeDAO.php

--- persistence/CrimeDAO.php
+++ persistence/CrimeDAO.php
@@ -1,10 +1,24 @@
 <?php
+<<<<<<< HEAD
+include_once(__APP_PATH.'/model/Crime.php');
+include_once(__APP_PATH.'/model/Tempo.php');
+include_once(__APP_PATH.'/model/Natureza.php');
+include_once(__APP_PATH.'/persistence/Conexao.php');
+include_once(__APP_PATH.'/persistence/NaturezaDAO.php');
+include_once(__APP_PATH.'/persistence/TempoDAO.php');
+include_once(__APP_PATH.'/exceptions/EConexaoFalha.php');
+include_once(__APP_PATH.'/exceptions/EcrimeListarTodosVazio.php');
+include_once(__APP_PATH.'/exceptions/ECrimeConsultarPorIdVazio.php');
+include_once(__APP_PATH.'/exceptions/ECrimeConsultarIdNaturezaVazio.php');
+include_once(__APP_PATH.'/exceptions/ECrimeConsultarIdTempoVazio.php');
+=======
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/model/Crime.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/model/Tempo.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/model/Natureza.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/persistence/Conexao.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/persistence/NaturezaDAO.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/mds2013/persistence/TempoDAO.php');
+>>>>>>> ea1ab785752134f47fcac9e22df0060c9d74d638
 class CrimeDAO{
 	private $conexao;
 	public function __construct(){
@@ -14,6 +28,9 @@ class CrimeDAO{
 	public function listarTodos(){
 		$sql = "SELECT * FROM crime";
 		$resultado = $this->conexao->banco->Execute($sql);
+		if(($resultado == null) || (empty($resultado) == true) || (count($resultado) == 0)){
+			throw new EcrimeListarTodosVazio();
+		}
 		while($registro = $resultado->FetchNextObject())
 		{
 			$dadosCrime = new Crime();
@@ -25,6 +42,9 @@ class CrimeDAO{
 	public function consultarPorId($id){
 		$sql = "SELECT * FROM crime WHERE id_crime = $id";
 		$resultado = $this->conexao->banco->Execute($sql);
+		if(($resultado == null) || (empty($resultado) == true) || (count($resultado) == 0)){
+			throw new ECrimeConsultarPorIdVazio();
+		}
 		$registro = $resultado->FetchNextObject();
 		$dadosCrime = new Crime();
 		$dadosCrime->__constructOverload($registro->ID_CRIME,$registro->TEMPO_ID_TEMPO,$registro->NATUREZA_ID_NATUREZA,$registro->QUANTIDADE);
@@ -33,6 +53,9 @@ class CrimeDAO{
 	public function consultarPorIdNatureza($id){
 		$sql = "SELECT * FROM crime WHERE id_natureza = $id";
 		$resultado = $this->conexao->banco->Execute($sql);
+		if(($resultado == null) || (empty($resultado) == true) || (count($resultado) == 0)){
+			throw new ECrimeConsultarIdNaturezaVazio();
+		}
 		$registro = $resultado->FetchNextObject();
 		$dadosCrime = new Crime();
 		$dadosCrime->__constructOverload($registro->ID_CRIME,$registro->TEMPO_ID_TEMPO,$registro->NATUREZA_ID_NATUREZA,$registro->QUANTIDADE);
@@ -41,6 +64,9 @@ class CrimeDAO{
 	public function consultarPorIdTempo($id){
 		$sql = "SELECT * FROM crime WHERE id_tempo = $id";
 		$resultado = $this->conexao->banco->Execute($sql);
+		if(($resultado == null) || (empty($resultado) == true) || (count($resultado) == 0)){
+			throw new ECrimeConsultarIdTempoVazio();
+		}
 		$registro = $resultado->FetchNextObject();
 		$dadosCrime = new Crime();
 		$dadosCrime->__constructOverload($registro->ID_CRIME,$registro->TEMPO_ID_TEMPO,$registro->NATUREZA_ID_NATUREZA,$registro->QUANTIDADE);
@@ -61,5 +87,8 @@ class CrimeDAO{
 	public function inserirCrime(Crime $crime){
 		$sql = "INSERT INTO crime (natureza_id_natureza,tempo_id_tempo,quantidade) VALUES ('{$crime->__getIdNatureza()}','{$crime->__getIdTempo()}','{$crime->__getQuantidade()}')";
 		$this->conexao->banco->Execute($sql);
+		if(!$this->banco->Connect($this->servidor,$this->usuario,$this->senha,$this->db)){
+				throw new EConexaoFalha();	
+		}
 	}
 }
