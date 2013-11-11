15bc9fcf7f181f91c0d549b81374e9f2a27e8f3b
Lucas Andrade <passaroalado@hotmail.com>
2013-11-11 19:25:36 -0200

    testes da classe RegiaoAdministrativa

--- test/model/RegiaoAdministrativaTeste.php
+++ test/model/RegiaoAdministrativaTeste.php
@@ -5,54 +5,17 @@
 	 * Classe RegiaoAdministrativa Teste
 	 **/
 	class RegiaoAdministrativaTeste extends PHPUnit_Framework_Testcase{
-		private $regiao_administrativa;
+		private $regiaoAdministrativa;
 		
-		public function testSetIdRegiaoAdministrivaExpectedValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setIDRegiaoAdministrativa(42);
-			$this->assertEquals(42, $regiao_administrativa->__getIdRegiaoAdministrativa());
+		public function testeSetIdRegiaoAdministriva(){
+			$regiaoAdministrativa = new RegiaoAdministrativa();
+			$regiaoAdministrativa->__setIdRegiaoAdministrativa(42);
+			$this->assertEquals(42, $regiaoAdministrativa->__getIdRegiaoAdministrativa());
 		}
-		
-		public function testSetIdRegiaoAdministrativaFloatValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setIDRegiaoAdministrativa(40.5);
-			$this->assertNull($regiao_administrativa->__getIdRegiaoAdministrativa());
-		}
-		
-		public function testSetIdRegiaoAdministrativaStringValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setIDRegiaoAdministrativa("id");
-			$this->assertNull($regiao_administrativa->__getIdRegiaoAdministrativa());
-		}
-		
-		public function testSetIdRegiaoAdministrativaNullValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setIDRegiaoAdministrativa(NULL);
-			$this->assertNull($regiao_administrativa->__getIdRegiaoAdministrativa());
-		}
-	
-		public function testSetNomeRegiaoAdminstrativaExpectedValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setNomeRegiao("Regiao Administrativa");
-			$this->assertEquals("Regiao Administrativa", $regiao_administrativa->__getNomeRegiao());
-		}
-		
-		public function testSetNomeRegiaoAdministrativaIntegerValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setNomeRegiao(108);
-			$this->assertNull($regiao_administrativa->__getNomeRegiao());
-		}
-		
-		public function testSetNomeRegiaoAdministrativaFloatValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setNomeRegiao(18.95);
-			$this->assertNull($regiao_administrativa->__getNomeRegiao());
-		}
-		
-		public function testSetNomeRegiaoAdministrativaNullValue(){
-			$regiao_administrativa = new RegiaoAdministrativa();
-			$regiao_administrativa->__setNomeRegiao(NULL);
-			$this->assertNull($regiao_administrativa->__getNomeRegiao());
+		public function testSetNomeRegiaoAdminstrativa(){
+			$regiaoAdministrativa = new RegiaoAdministrativa();
+			$regiaoAdministrativa->__setNomeRegiao("Regiao Administrativa");
+			$this->assertEquals("Regiao Administrativa", $regiaoAdministrativa->__getNomeRegiao());
 		}
 	}
 ?>
