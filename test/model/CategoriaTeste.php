6e33d455526d2a239c72e75c295831a3f03f8268
Lucas Andrade <passaroalado@hotmail.com>
2013-11-11 19:05:26 -0200

    testes da classe Categoria e da classe Crime

--- test/model/CategoriaTeste.php
+++ test/model/CategoriaTeste.php
@@ -5,16 +5,23 @@
 	 * Classe Categoria Teste
 	 **/
 	class CategoriaTeste extends PHPUnit_Framework_Testcase{
-		public function __testeSetIdCategoria(){
+		public function testeSetIdCategoria(){
 			$categoria = new Categoria();
 			$categoria->__setIdCategoria(10);
 			$this->assertEquals(10, $categoria->__getIdCategoria());
 		}
 		
-		public function _testeSetNomeCategoria(){
+		public function testeSetNomeCategoria(){
 			$categoria = new Categoria();
 			$categoria->__setNomeCategoria("NomeCategoria");
-			$this->assertEquals("NomeCategria", $categoria->__getNomeCategoria());
+			$this->assertEquals("NomeCategoria", $categoria->__getNomeCategoria());
+		}
+		public function testeConstructOverLoad(){
+			$categoria = new Categoria();
+			$categoria->__constructOverload(2,"nomeCategoria");
+			$this->assertEquals(2,$categoria->__getIdCategoria());
+			$this->assertEquals("nomeCategoria",$categoria->__getNomeCategoria());
+			
 		}
 	}
 
--- test/model/CrimeTeste.php
+++ test/model/CrimeTeste.php
@@ -10,61 +10,29 @@
 			$crime = new Crime();
 			$crime->__setIdCrime(15);
 			$this->assertEquals(15, $crime->__getIdCrime());
-			$crime = new Crime();
-			$crime->__setIdCrime(10);
-			$this->assertEquals(10, $crime->__getIdCrime());
-			$crime = new Crime();
-			$crime->__setIdCrime(100);
-			$this->assertEquals(100, $crime->__getIdCrime());
-			$crime = new Crime();
-			$crime->__setIdCrime(50);
-			$this->assertEquals(50, $crime->__getIdCrime());
-				
 		}
 		public function testeAtributoQuantidade(){
 			$crime = new Crime();
 			$crime->__setQuantidade(15);
 			$this->assertEquals(15, $crime->__getQuantidade());
-			$crime = new Crime();
-			$crime->__setQuantidade(10);
-			$this->assertEquals(10, $crime->__getQuantidade());
-			$crime = new Crime();
-			$crime->__setQuantidade(100);
-			$this->assertEquals(100, $crime->__getQuantidade());
-			$crime = new Crime();
-			$crime->__setQuantidade(50);
-			$this->assertEquals(50, $crime->__getQuantidade());
 		}
 		public function testeIdTempo(){
 			$crime = new Crime();
 			$crime->__setIdTempo(15);
 			$this->assertEquals(15, $crime->__getIdTempo());
-			$crime = new Crime();
-			$crime->__setIdTempo(15);
-			$this->assertEquals(15, $crime->__getIdTempo());
-			$crime = new Crime();
-			$crime->__setIdTempo(10);
-			$this->assertEquals(10, $crime->__getIdTempo());
-			$crime = new Crime();
-			$crime->__setIdTempo(100);
-			$this->assertEquals(100, $crime->__getIdTempo());
-			$crime = new Crime();
-			$crime->__setIdTempo(50);
-			$this->assertEquals(50, $crime->__getIdTempo());
 		}
 		public function testeIdNatureza(){
 			$crime = new Crime();
 			$crime->__setIdNatureza(15);
 			$this->assertEquals(15, $crime->__getIdNatureza());
+		}
+		public function testeConstructOverLoad(){
 			$crime = new Crime();
-			$crime->__setIdNatureza(10);
-			$this->assertEquals(10, $crime->__getIdNatureza());
-			$crime = new Crime();
-			$crime->__setIdNatureza(100);
-			$this->assertEquals(100, $crime->__getIdNatureza());
-			$crime = new Crime();
-			$crime->__setIdNatureza(50);
-			$this->assertEquals(50, $crime->__getIdNatureza());
+			$crime->__constructOverload(1,2,3,4);
+			$this->assertEquals(1, $crime->__getIdCrime());
+			$this->assertEquals(2, $crime->__getIdTempo());
+			$this->assertEquals(3, $crime->__getIdNatureza());
+			$this->assertEquals(4, $crime->__getQuantidade());
 		}
 	}
 ?>
