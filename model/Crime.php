3e9d50d55ed4064c34db6bdf7f7b73c4a9510bbb
Lucas Andrade <passaroalado@hotmail.com>
2013-11-11 17:29:03 -0200

    excessoes e testes da model Crime

--- model/Crime.php
+++ model/Crime.php
@@ -1,30 +1,61 @@
 <?php
+include_once('/../exceptions/EFalhaLeituraSerieCrime.php');
+
 class Crime{
 	private $idCrime;
 	private $quantidade;
 	private $idTempo;
 	private $idNatureza;
+	private $exceptionCrime;
 	
 	public function __setIdCrime($idCrime){
-		$this->idCrime = $idCrime;
+		$exceptionCrime = new EFalhaLeituraSerieCrime();
+		if(!is_int($idCrime)){
+			throw $exceptionCrime;
+			echo $exceptionCrime->getMessage();
+		}
+		else {
+			$this->idCrime = $idCrime;
+		}
 	}
 	public function __getIdCrime(){
 		return $this->idCrime;
 	}
 	public function __setQuantidade($quantidade){
-		$this->quantidade = $quantidade;
+		$exceptionCrime = new EFalhaLeituraSerieCrime();
+		if( (!is_int($quantidade)) || (is_float($quantidade)) ){
+			throw $exceptionCrime;
+			echo $exceptionCrime->getMessage();
+		}
+		else{
+			$this->quantidade = $quantidade;
+		}
 	}
 	public function __getQuantidade(){
 		return $this->quantidade;
 	}
 	public function __setIdTempo($idTempo){
-		$this->idTempo = $idTempo;
+		$exceptionCrime = new EFalhaLeituraSerieCrime();
+		if((!is_int($idTempo)) || (is_float($idTempo))){
+			throw $exceptionCrime;
+			echo $exceptionCrime->getMessage();
+		}
+		else{
+			$this->idTempo = $idTempo;
+		}
 	}
 	public function __getIdTempo(){
 		return $this->idTempo;
 	}
 	public function __setIdNatureza($idNatureza){
-		$this->idNatureza = $idNatureza;
+		$exceptionCrime = new EFalhaLeituraSerieCrime();
+		if(!is_int($idNatureza)){
+			throw $exceptionCrime;
+			echo $exceptionCrime->getMessage();
+		}
+		else{
+			$this->idNatureza = $idNatureza;
+		}
 	}
 	public function __getIdNatureza(){
 		return $this->idNatureza;
--- test/model/CrimeTeste.php
+++ test/model/CrimeTeste.php
@@ -40,32 +40,32 @@
 		public function testeAtributoQuantidade(){
 			$crime = new Crime();
 			$crime->__setQuantidade(15);
-			$this->assertEquals(15, $crime->__getIdCrime());
+			$this->assertEquals(15, $crime->__getQuantidade());
 			$crime = new Crime();
 			$crime->__setQuantidade("asdf");
-			$this->assertEquals(null, $crime->__getIdCrime());
+			$this->assertEquals(null, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime(14.9);
-			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime->__setQuantidade(14.9);
+			$this->assertEquals(null, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime($this);
-			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime->__setQuantidade($this);
+			$this->assertEquals(null, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime($crime);
-			$this->assertEquals(null, $crime->__getIdCrime());
+			$crime->__setQuantidade($crime);
+			$this->assertEquals(null, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime(10);
-			$this->assertEquals(10, $crime->__getIdCrime());
+			$crime->__setQuantidade(10);
+			$this->assertEquals(10, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime(100);
-			$this->assertEquals(100, $crime->__getIdCrime());
+			$crime->__setQuantidade(100);
+			$this->assertEquals(100, $crime->__getQuantidade());
 			$crime = new Crime();
-			$crime->__setIdCrime(0);
-			$this->assertEquals(NULL, $crime->__getIdCrime());
+			$crime->__setQuantidade(0);
+			$this->assertEquals(NULL, $crime->__getQuantidade());
 			$this->assertEquals(NULL, 0);
 			$crime = new Crime();
-			$crime->__setIdCrime(50);
-			$this->assertEquals(50, $crime->__getIdCrime());
+			$crime->__setQuantidade(50);
+			$this->assertEquals(50, $crime->__getQuantidade());
 		}
 		public function testeIdTempo(){
 			$crime = new Crime();
@@ -101,9 +101,6 @@
 			$this->assertEquals(50, $crime->__getIdTempo());
 		}
 		public function testeIdNatureza(){
-			$crime = new Crime();
-			$crime->__setIdNatureza(15);
-			$this->assertEquals(15, $crime->__getIdNatureza());
 			$crime = new Crime();
 			$crime->__setIdNatureza(15);
 			$this->assertEquals(15, $crime->__getIdNatureza());
