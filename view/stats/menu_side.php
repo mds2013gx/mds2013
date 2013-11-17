c968137a7360c433f5bc54fc8d557f63411ab449
Eduardo Augusto <eduardo_augusto2002@yahoo.com.br>
2013-11-17 17:54:21 -0200

    Inicio da migracao da nova interface

---
+++ view/stats/menu_side.php
@@ -0,0 +1,26 @@
+<!-- start: Header -->
+	
+		<div class="container-fluid-full">
+		<div class="row-fluid">
+				
+			<!-- start: Main Menu -->
+			<div id="sidebar-left" class="span2">
+				
+				<div class="nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li><a href="index.php"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>	
+						<li><a href="ui.php"><i class="icon-eye-open"></i><span class="hidden-tablet"> Recursos da IU</span></a></li>
+						<li><a href="widgets.php"><i class="icon-dashboard"></i><span class="hidden-tablet"> Widgets</span></a></li>
+						<li>
+							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Paginas de Exemplo</span> <span class="label">2</span></a>
+							<ul>
+								<li><a class="submenu" href="infra.php"><i class="icon-hdd"></i><span class="hidden-tablet"> Infraestrutura</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-tasks"></i><span class="hidden-tablet"> Tarefas</span></a></li>
+							</ul>	
+						</li>
+						<li><a href="chart.php"><i class="icon-list-alt"></i><span class="hidden-tablet"> Gráficos</span></a></li>
+						<li><a href="table.php"><i class="icon-align-justify"></i><span class="hidden-tablet"> Tabelas</span></a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- end: Main Menu -->
