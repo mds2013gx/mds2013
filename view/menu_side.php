f382cf8c5b798fd0fd245f6971f1d957e00dfc02
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2013-11-23 14:01:15 -0200

    Ajuste de menus com novas consultas

--- view/menu_side.php
+++ view/menu_side.php
@@ -8,18 +8,27 @@
 				
 				<div class="nav-collapse sidebar-nav">
 					<ul class="nav nav-tabs nav-stacked main-menu">
-						<li><a href="index.php"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>	
-						<li><a href="ui.php"><i class="icon-eye-open"></i><span class="hidden-tablet"> Recursos da IU</span></a></li>
-						<li><a href="widgets.php"><i class="icon-dashboard"></i><span class="hidden-tablet"> Widgets</span></a></li>
+						<li><a href="chart.php"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Estatísticas</span></a></li>	
 						<li>
-							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Paginas de Exemplo</span> <span class="label">2</span></a>
+							<a class="dropmenu" href="#" alt="Por Natureza" title="Por Natureza"><i class="icon-globe"></i><span class="hidden-tablet"> Crimes</span> <span class="label">5</span></a>
 							<ul>
-								<li><a class="submenu" href="infra.php"><i class="icon-hdd"></i><span class="hidden-tablet"> Infraestrutura</span></a></li>
-								<li><a class="submenu" href="tasks.php"><i class="icon-tasks"></i><span class="hidden-tablet"> Tarefas</span></a></li>
-							</ul>	
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Mês</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Bimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Trimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Quadrimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Ano</span></a></li>
+							</ul>
 						</li>
-						<li><a href="chart.php"><i class="icon-list-alt"></i><span class="hidden-tablet"> Gráficos</span></a></li>
-						<li><a href="table.php"><i class="icon-align-justify"></i><span class="hidden-tablet"> Tabelas</span></a></li>
+						<li>
+							<a class="dropmenu" href="#" alt="Região Administrativa" title="Região Administrativa"><i class="icon-move"></i><span class="hidden-tablet"> Cidades</span> <span class="label">4</span></a>
+							<ul>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Mês</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Bimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Trimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Quadrimestre</span></a></li>
+								<li><a class="submenu" href="tasks.php"><i class="icon-calendar"></i><span class="hidden-tablet"> Por Ano</span></a></li>
+							</ul>
+						</li>
 					</ul>
 				</div>
 			</div>
