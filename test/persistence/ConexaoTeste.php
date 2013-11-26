0ab54f5676dda8e2138afd3f3415b8e5ad53ef6e
Lucas Andrade <passaroalado@hotmail.com>
2013-11-26 17:03:47 -0200

    teste da classe Conexao

---
+++ test/persistence/ConexaoTeste.php
@@ -0,0 +1,16 @@
+<?php
+require_once('C:/xampp/htdocs/mds2013/persistence/Conexao.php');
+
+/**
+ * Classe de teste da classe Conexao
+ * @author Lucas Andrade Ribeiro
+ * @copyright RadarCriminal 2013
+ */
+class ConexaoTeste extends PHPUnit_Framework_TestCase{
+
+	public function testConstruct(){
+		$conexao = new Conexao();
+		$this->assertObjectHasAttribute('banco', $conexao);
+		$this->assertInstanceOf('Conexao', $conexao);
+	}
+}
