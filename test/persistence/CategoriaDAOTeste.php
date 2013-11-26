aebc017715b55010348b2d70490066e1a0ad4e4c
Lucas Andrade <passaroalado@hotmail.com>
2013-11-25 23:34:53 -0200

    teste da camada DAO

--- test/persistence/CategoriaDAOTeste.php
+++ test/persistence/CategoriaDAOTeste.php
@@ -1,27 +1,53 @@
 <?php
 	require_once('C:/xampp/htdocs/mds2013/persistence/CategoriaDAO.php');
+	include_once('C:/xampp/htdocs/mds2013/persistence/Conexao.php');
 	
 	/**
 	 * Classe CategoriaDAOTeste
 	 **/
-	class CategoriaDAOTeste extends PHPUnit_Framework_Testcase{
-	
+	class CategoriaDAOTeste extends PHPUnit_Framework_TestCase{
+		
+		
+		public function testConstruct(){
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
+		}
 		public function testListarTodas()
 		{
-		
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
+			$this->assertNotEmpty($categoriaDAO->listarTodas());
+			$this->assertNotNull($categoriaDAO->listarTodas());
 		}
 		
 		public function testListarTodasAlfabeticamente()
 		{
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
+			$this->assertNotEmpty($categoriaDAO->listarTodasAlfabicamente());
+			$this->assertNotNull($categoriaDAO->listarTodasAlfabicamente());
 		}
 	
 		public function testConsultarPorId(){
-		
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
+			$this->assertInstanceOf('Categoria', $categoriaDAO->consultarPorId(1));
+			$this->assertObjectHasAttribute('idCategoria',$categoriaDAO->consultarPorId(1));
 		}
 		public function testConsultarPorNome(){
-			
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
+			$this->assertInstanceOf('Categoria', $categoriaDAO->consultarPorNome('Criminalidade'));
+			$this->assertObjectHasAttribute('idCategoria',$categoriaDAO->consultarPorNome('Criminalidade'));
 		}
 		public function testeInserirCategoria(){
-			
+			$categoriaDAO = new CategoriaDAO();
+			$this->assertObjectHasAttribute('conexao', $categoriaDAO);
+			$this->assertInstanceOf('CategoriaDAO', $categoriaDAO);
 		}
 	}
