93c5445df4fd3b8359800e8140f74a58d7bb9094
Lucas Andrade <passaroalado@hotmail.com>
2013-11-27 11:54:53 -0200

    testes de funcoes e de exceptions da classe Categoria

--- test/model/CategoriaTeste.php
+++ test/model/CategoriaTeste.php
@@ -5,23 +5,51 @@
 	 * Classe Categoria Teste
 	 **/
 	class CategoriaTeste extends PHPUnit_Framework_Testcase{
-		public function testeSetIdCategoria(){
+			
+		public function testSetIdCategoria(){
 			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
 			$categoria->__setIdCategoria(10);
 			$this->assertEquals(10, $categoria->__getIdCategoria());
 		}
-		
-		public function testeSetNomeCategoria(){
+		public function testExceptionSetIdCategoria(){
 			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
+			$this->setExpectedException('ETipoErrado');
+			$categoria->__setIdCategoria('errado');
+		}
+		public function testSetNomeCategoria(){
+			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
 			$categoria->__setNomeCategoria("NomeCategoria");
 			$this->assertEquals("NomeCategoria", $categoria->__getNomeCategoria());
 		}
-		public function testeConstructOverLoad(){
+		public function testExceptionSetNomeCategoria(){
 			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
+			$this->setExpectedException('ETipoErrado');
+			$categoria->__setNomeCategoria(13);
+		}
+		
+		public function testConstructOverLoad(){
+			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
 			$categoria->__constructOverload(2,"nomeCategoria");
 			$this->assertEquals(2,$categoria->__getIdCategoria());
 			$this->assertEquals("nomeCategoria",$categoria->__getNomeCategoria());
 			$this->assertInstanceOf('Categoria', $categoria);
+		}
+		public function testExceptionConstructOverLoad(){
+			$categoria = new Categoria();
+			$this->assertInstanceOf('Categoria',$categoria);
+			$this->assertObjectHasAttribute('idCategoria', $categoria);
+			$this->setExpectedException('ETipoErrado');
+			$categoria->__constructOverload("erro",1);
 		}
 	}
 
