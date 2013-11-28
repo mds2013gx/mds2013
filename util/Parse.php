bda0b67ce772dca6c773165171a8cacb81ef34a4
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-11-28 12:21:46 -0200

    Implementacao do Parse por RA, coletagem das regioes

--- util/Parse.php
+++ util/Parse.php
@@ -14,6 +14,7 @@ class Parse{
 	private $categoria;
 	private $dados;
 	private $total;
+	private $regiao;
 	
 	public function __construct($planilha){
 		try{
@@ -162,32 +163,60 @@ class Parse{
 		* Loop para pegar os nomes das naturezas de crimes contidas na planilha de RA
 		* @author Lucas Carvalho 
 		*/
-		for($i=8,$auxNatureza=0;$i<45;$i++){
+		for($i=0,$auxNatureza=0;$i<45;$i++){
 		 		// Val Ã© o valor da cÃ©lula que esta sendo armazenado na nova tabela val(linha, coluna, sheet)
 		 		if($i>7 && $i<11){
 		 			$this->natureza[$this->__getCategoria()[0]][$auxNatureza] =  $this->dados->val($i,'B',1);
 		 			$auxNatureza++;
 		 		}else if(($i>11 && $i<26) || ($i>26 && $i<31)){
-		 			$this->natureza[$this->__getCategoria()[1]][$auxNatureza] =  $this->dados->val($i,'B',2);
+		 			$this->natureza[$this->__getCategoria()[1]][$auxNatureza] =  $this->dados->val($i,'B',1);
 		 			$auxNatureza++;
 		 		}else if($i>33 && $i<36){
-		 			$this->natureza[$this->__getCategoria()[2]][$auxNatureza] =  $this->dados->val($i,'B',2);
+		 			$this->natureza[$this->__getCategoria()[2]][$auxNatureza] =  $this->dados->val($i,'B',1);
 		 			$auxNatureza++;
 		 		}else if($i>37 && $i<42){
-		 			$this->natureza[$this->__getCategoria()[3]][$auxNatureza] =  $this->dados->val($i,'B',2);
+		 			$this->natureza[$this->__getCategoria()[3]][$auxNatureza] =  $this->dados->val($i,'B',1);
 		 			$auxNatureza++;
 		 		}else if($i>42 && $i<45){
-		 			$this->natureza[$this->__getCategoria()[4]][$auxNatureza] =  $this->dados->val($i,'B',2);
+		 			$this->natureza[$this->__getCategoria()[4]][$auxNatureza] =  $this->dados->val($i,'B',1);
 		 			$auxNatureza++;
 		 		}else{
 		 			continue;
 		 		}
 		}
+
 		/**
 		 * Loop para pegar os nomes dos tempos contidas na planilha de RA
 		 * @author Lucas Carvalho
 		 */
-		
+		$this->tempo[0] = $this->dados->val(7,7,1); 
+
+		/**
+		* Loop para pegar os nomes das regi�es contidas na planilha RA nas linhas 
+		* @author Lucas Carvalho
+		*/
+		for($i=0, $auxRegiao = 0; $i<3; $i++ ){
+			if ($i=0) {
+				$linha = 6;
+				$numeroColunas = 25;
+			}else if($i=55){
+				$linha = 55;
+				$numeroColunas = 25;
+			}else{
+				$linha = 104;
+				$numeroColunas = 29;
+			}
+			for ($j=0;$j<$numeroColunas ; $j++) { 
+				if(($j<6) && ($j % 2 !=0)){
+					continue;
+				}else{
+					$this->regiao[$auxRegiao] = $this->dados->val($linha,$j,1);
+					$auxRegiao++;
+				}
+			}
+		}
+
+
 	}
 	/**
 	*	Desenvolvimento do m�todo para efetuar parse da planilha de quadrimestre
