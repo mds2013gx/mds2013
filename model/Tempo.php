960d97202e026ebd13de34e4f7f08f6a2bed26a7
Lucas Andrade <passaroalado@hotmail.com>
2013-12-02 03:28:27 -0200

    refatoracao da classe Tempo

--- model/Tempo.php
+++ model/Tempo.php
@@ -1,25 +1,56 @@
 <?php
+include_once('C:/xampp/htdocs/mds2013/exceptions/ETipoErrado.php');
 class Tempo{
 	private $idTempo;
 	private $intervalo;
+	private $mes;
+	
 	
 	public function __setIdTempo($idTempo){
+		if(!is_numeric($idTempo)){
+			throw new ETipoErrado();
+		}
 		$this->idTempo = $idTempo;
 	}
 	public function __getIdTempo(){
-		return $this->idTempo;
+		$retorno = $this->idTempo;
+		if(!is_numeric($idTempo)){
+			throw new ETipoErrado();
+		}
+		return $retorno;
 	}
 	public function __setIntervalo($intervalo){
+		if(!is_numeric($intervalo)){
+			throw new ETipoErrado();
+		}
 		$this->intervalo = $intervalo;
 	}
 	public function __getIntervalo(){
-		return $this->intervalo;
+		$retorno = $this->intervalo;
+		if(!is_numeric($retorno)){
+			throw new ETipoErrado();
+		}
+		return $retorno;
+	}
+	public function __setMes($mes){
+		if(!is_string($mes)){
+			throw new ETipoErrado();
+		}
+		$this->mes = $mes;
+	}
+	public function __getMes(){
+		$retorno =  $this->Mes;
+		if(!is_string($retorno)){
+			throw new ETipoErrado();
+		}
+		return  $retorno;
 	}
 	public function __construct(){
 		
 	}
-	public function __constructOverload($idTempo,$intervalo){
+	public function __constructOverload($idTempo,$intervalo,$mes){
 		$this->idTempo = $idTempo;
 		$this->intervalo = $intervalo;
+		$this->mes = $mes;
 	}
 }
