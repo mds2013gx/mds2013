61a922781dcc10ca6bd89441a8fa215f405892c5
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2013-12-02 13:36:38 -0200

    Implementacao do metodo inserirCrimeArrayParseQuadrimestral e RA

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
- include ('C:/xampp/htdocs/mds2013/util/RunParse.php');
 
+ include ('C:/xampp/htdocs/mds2013/util/Parse.php'); 
+ 
  $parse = new Parse("JAN_SET_2011_12  POR REGIAO ADM_2.xls");
- //$parse = new Parse("Quadrimestre_final.2013.xls");
- $runParse = new RunParse();
-
+ //print_r(array_keys($parse->__getCrime()));
+ $natureza = key($parse->__getCrime());
+ $regiao = key($parse->__getCrime()[$natureza]);
+ $tempo = key($parse->__getCrime()[$natureza][$regiao]);
+ $crime = $parse->__getCrime()[$natureza][$regiao][$tempo];
+ echo $crime;
 ?>
